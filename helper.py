fe379158649be028b430e94c0fd211d9bea21ff4
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-10-06 21:50:10 -0400

    Fix helper.py

--- helper.py
+++ helper.py
@@ -4,6 +4,16 @@ class Gen5Save:
         if isinstance(data, BufferedReader):
             data.seek(0x19404)
             tname = data.read(16) # TODO: is this actually the proper trainer name length
+            # Now we need to do some post processing
+            tname = tname.strip(b"\x00") # Strip \x00
+            tname = tname.strip(b"\xff") # Strip \xff
+            outstring = b""
+            for b in tname: # Strip the \x00s inbetween letters
+                byte = b.to_bytes(1, 'little')
+                if byte != b"\x00":
+                    outstring = outstring + byte
+            # Now we make it a string.
+            tname = str(outstring)[2:-1] # The [2:-1] part strips the annoying b"" things from the string
             self.trainer_name = tname
         elif isinstance(data, bytes):
             self._data = data
