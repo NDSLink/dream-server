97d523aafebfab242b84953c685253ebb3a73b4a
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-10-09 10:19:15 -0400

    A lot of fixes in models

--- models.py
+++ models.py
@@ -1,46 +1,48 @@
 from app import db
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
 class GSUser(db.Model):
     __tablename__ = 'gsuser'
-    id = Column(String, primary_key=True) # TODO: create a frontend where you can see GS related info, and download your save, and maybe even edit it
+    id = db.Column(db.String(), primary_key=True) # TODO: create a frontend where you can see GS related info, and download your save, and maybe even edit it
     # NOTE: this should be pretty easy to implement
     sleeping_pokemon = relationship("Pokemon")
+    name = db.Column(db.String(7))
+    poke_is_sleeping = db.Column(db.Boolean())
+    tid = db.Column(db.Integer)
     # TODO: What else?
     def __repr__(self):
         return '<GSUser %r>' % self.id
 
 class Pokemon(db.Model):
-    id = Column(Integer, primary_key=True)
-    dexno = Column(Integer)
-    name = Column(String)
-    level = Column(Integer)
-    exp = Column(Integer)
-    iv_hp = Column(Integer)
-    iv_atk = Column(Integer)
-    iv_def = Column(Integer)
-    iv_spatk = Column(Integer)
-    iv_spdef = Column(Integer)
-    iv_speed = Column(Integer)
-    ev_hp = Column(Integer)
-    ev_atk = Column(Integer)
-    ev_def = Column(Integer)
-    ev_spatk = Column(Integer)
-    ev_spdef = Column(Integer)
-    ev_speed = Column(Integer)
-    nature = Column(String)
-    ability = Column(String)
-    item = Column(String)
-    move1 = Column(String)
-    move2 = Column(String)
-    move3 = Column(String)
-    move4 = Column(String)
-    gsuser_id = Column(String, ForeignKey('gsuser.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    dexno = db.Column(db.Integer)
+    name = db.Column(db.String(16))
+    level = db.Column(db.Integer)
+    exp = db.Column(db.Integer)
+    iv_hp = db.Column(db.Integer)
+    iv_atk = db.Column(db.Integer)
+    iv_def = db.Column(db.Integer)
+    iv_spatk = db.Column(db.Integer)
+    iv_spdef = db.Column(db.Integer)
+    iv_speed = db.Column(db.Integer)
+    ev_hp = db.Column(db.Integer)
+    ev_atk = db.Column(db.Integer)
+    ev_def = db.Column(db.Integer)
+    ev_spatk = db.Column(db.Integer)
+    ev_spdef = db.Column(db.Integer)
+    ev_speed = db.Column(db.Integer)
+    nature = db.Column(db.String())
+    ability = db.Column(db.String())
+    item = db.Column(db.String())
+    move1 = db.Column(db.String())
+    move2 = db.Column(db.String())
+    move3 = db.Column(db.String())
+    move4 = db.Column(db.String())
+    gsuser_id = db.Column(db.String(), db.ForeignKey('gsuser.id'))
     # TODO: Convert moves into valid pokeapi.cc move names
     # i.e., V-Create becomes v-create, Ice Burn becomes ice-burn
     # There are likely special exceptions to that
     # NOTE:
-    # pkapiified_str = 'whatever string'.lower().replace(' ', '-')
+    # pkapiified_str = 'whatever db.String()'.lower().replace(' ', '-')
     # should work?
 
 
