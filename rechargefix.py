6c45d63225863c74a50116c6370325d9f76eb1ca
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-10-10 15:17:04 -0400

    Add fixer script for the recharge bug

---
+++ rechargefix.py
@@ -0,0 +1,34 @@
+import models
+from app import db
+
+print("---Recharge Fixer---")
+print("""Currently, there is a bug in Dream Land that causes Dream World
+To occasionally complain that "it must recharge". This script will solve 
+that.""")
+is_sleeping = input("Is there currently a sleeping Pokemon in this save? (y/N)").lower().startswith("y")
+tid = input("What is this save's TID?")
+print("Please wait...")
+u = models.GSUser.query.filter_by(tid=tid)
+is_right = input(f"The trainer's name is {u.name}, right? (y/N)").lower().startswith("y")
+
+if is_right:
+    if u.poke_is_sleeping == is_sleeping:
+        print("Error! The user's save is synced correctly. Wait one day, then try Game Sync again.")
+        exit(1)
+    u.poke_is_sleeping = is_sleeping
+    try:
+        db.session.add(u)
+    except Exception as e:
+        print(f"Error! Unable to add modded user. Error:{e}")
+        exit(3)
+    try:
+        db.session.commit()
+    except Exception as e:
+        print(f"Error! Unable to commit modded user. Error:{e}")
+        exit(4)
+    exit(0)
+else:
+    print("Oh? There must of been a database related error.")
+    exit(2)
+
+    
