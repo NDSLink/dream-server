114c4b82108434eae67cc7e3a6fd1d38c48138a7
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-10-10 15:23:11 -0400

    Fix the fix

--- rechargefix.py
+++ rechargefix.py
@@ -5,11 +5,14 @@ print("---Recharge Fixer---")
 print("""Currently, there is a bug in Dream Land that causes Dream World
 To occasionally complain that "it must recharge". This script will solve 
 that.""")
-is_sleeping = input("Is there currently a sleeping Pokemon in this save? (y/N)").lower().startswith("y")
-tid = input("What is this save's TID?")
+is_sleeping = input("Is there currently a sleeping Pokemon in this save? (y/N) ").lower().startswith("y")
+tid = int(input("What is this save's TID? "))
 print("Please wait...")
-u = models.GSUser.query.filter_by(tid=tid)
-is_right = input(f"The trainer's name is {u.name}, right? (y/N)").lower().startswith("y")
+u = models.GSUser.query.filter_by(id=tid).first()
+if u == None:
+    print("Huh? There is no user with that TID.")
+    exit(5)
+is_right = input(f"The trainer's name is {u.name}, right? (y/N) ").lower().startswith("y")
 
 if is_right:
     if u.poke_is_sleeping == is_sleeping:
