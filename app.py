7b18f8f7041967d39a656ec5cd3e8e0762039b5c
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-10-23 20:11:49 -0400

    Document some things that I forgot to document

--- app.py
+++ app.py
@@ -2,16 +2,16 @@
 BASE_RESPONSE = b"\x00\x00\x00\x00" + (b"\x00" * 0x7C)
 END_RESPOSNE = b"\xFF" * 0x40
 PLAYSTATUS = (
-    "account.playstatus"  # Likely checking if you can play dreamworld at the moment
+    "account.playstatus"  # Called when you perform Game Sync
 )
-SLEEPILY_BITLIST = "sleepily.bitlist"  # ???
+SLEEPILY_BITLIST = "sleepily.bitlist"  # List of banned species
 SAVEDATA_GETBW = "savedata.getbw"  # B2W2 Memory Link
 SAVEDATA_DOWNLOAD = "savedata.download"  # self-explanatory
 WORLDBATTLE_DOWNLOAD = (
     "worldbattle.download"  # Click Battle Competition>Wi-Fi Competition>Participate
 )
 ACCOUNT_CREATEDATA = "account.createdata"  # Unused
-ACCOUNT_CREATE_UPLOAD = "account.create.upload"  # ???
+ACCOUNT_CREATE_UPLOAD = "account.create.upload"  # Upload savedata, alias to savedata.upload
 SAVEDATA_UPLOAD = "savedata.upload"  # self-explanatory
 WORLDBATTLE_UPLOAD = "worldbattle.upload"  # ???
 SAVEDATA_DOWNLOAD_FINISH = "savedata.download.finish"  # likely telling the server that savedata download is done
