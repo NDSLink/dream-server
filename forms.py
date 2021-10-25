107fc8724683f1781121dea4324b02a3084f1886
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-10-25 19:18:38 -0400

    Add forms

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
+    submit = SubmitField('Login')
+
+class LinkForm(FlaskForm):
+    gsid = StringField('Game Sync ID')
+    submit = SubmitField('Link!')
