7344200742ae54884932d4d8a7e1a91fd7814584
zurgeg <46549042+zurgeg@users.noreply.github.com>
2023-10-20 09:41:09 -0400

    Add app context to rechargefix No clue why this didn't raise an error earlier

--- rechargefix.py
+++ rechargefix.py
@@ -1,4 +1,4 @@
-from app import db
+from app import db, app
 import models
 
 print("---Recharge Fixer---")
@@ -14,32 +14,33 @@ is_sleeping = (
 )
 tid = int(input("What is this save's TID? "))
 print("Please wait...")
-u = models.GSUser.query.filter_by(tid=tid).first()
-if u == None:
-    print("Huh? There is no user with that TID.")
-    exit(5)
-is_right = (
-    input(f"The trainer's name is {u.name}, right? (y/N) ").lower().startswith("y")
-)
+with app.app_context():
+    u = models.GSUser.query.filter_by(tid=tid).first()
+    if u == None:
+        print("Huh? There is no user with that TID.")
+        exit(5)
+    is_right = (
+        input(f"The trainer's name is {u.name}, right? (y/N) ").lower().startswith("y")
+    )
 
-if is_right:
-    if u.poke_is_sleeping == is_sleeping:
-        print(
-            "Error! The user's save is synced correctly. Wait one day, then try Game Sync again."
-        )
-        exit(1)
-    u.poke_is_sleeping = is_sleeping
-    try:
-        db.session.add(u)
-    except Exception as e:
-        print(f"Error! Unable to add modded user. Error:{e}")
-        exit(3)
-    try:
-        db.session.commit()
-    except Exception as e:
-        print(f"Error! Unable to commit modded user. Error:{e}")
-        exit(4)
-    exit(0)
-else:
-    print("Oh? There must of been a database related error.")
-    exit(2)
+    if is_right:
+        if u.poke_is_sleeping == is_sleeping:
+            print(
+                "Error! The user's save is synced correctly. Wait one day, then try Game Sync again."
+            )
+            exit(1)
+        u.poke_is_sleeping = is_sleeping
+        try:
+            db.session.add(u)
+        except Exception as e:
+            print(f"Error! Unable to add modded user. Error:{e}")
+            exit(3)
+        try:
+            db.session.commit()
+        except Exception as e:
+            print(f"Error! Unable to commit modded user. Error:{e}")
+            exit(4)
+        exit(0)
+    else:
+        print("Oh? There must've been a database related error.")
+        exit(2)
